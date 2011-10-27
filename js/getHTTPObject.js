84e698b62834db5eab60d14d1ff308ae6c3bdc3c
wrj913 <wrj913@gmail.com>
2011-10-27 09:15:02 +0800

    add ajax create method

---
+++ js/getHTTPObject.js
@@ -0,0 +1,15 @@
+function getHTTPObject() {
+
+	if(( typeof(XMLHttpRequest) == "object")|| (typeof(XMLHttpRequest) == "undefined")) {	
+		XMLHttpRequest=function(){ 		
+			try { return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+				catch(e){}
+			try { return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+				catch(e){}
+			try { return new ActiveXObject("Msxml2.XMLHTTP");}
+				catch(e){}
+			return false;
+		};		
+	}	
+	return new XMLHttpRequest();
+}
